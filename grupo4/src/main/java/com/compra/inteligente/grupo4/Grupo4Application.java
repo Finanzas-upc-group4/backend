a43ea823f598ae9a13dd4d56c657ba829dde83bb
Diego Santivañez <Diegojsf17@gmail.com>
2023-11-22 10:29:48 -0500

    feat: swagger ui implemented

--- grupo4/src/main/java/com/compra/inteligente/grupo4/Grupo4Application.java
+++ grupo4/src/main/java/com/compra/inteligente/grupo4/Grupo4Application.java
@@ -3,6 +3,8 @@ package com.compra.inteligente.grupo4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
